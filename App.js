daf1b9f2fbb5734b50d65b57de168106fae7d59d
Nilo Marcos <nilo.n2m15@gmail.com>
2021-03-16 11:41:56 -0300

    Envio App

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+
+  function calcular(){
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+    // alert(imc)
+
+    if(imc < 18.6){
+      alert('Voce esta abaixo do peso! ' + imc.toFixed(2));
+    } else if(imc >= 18.6 && imc < 24.9){
+      alert('Voce esta no peso ideal! ' + imc.toFixed(2));
+    } else if(imc >= 24.9 && imc < 34.9){
+      alert('Voce esta levemente acima do peso! ' + imc.toFixed(2))
+    } else if(imc >= 34.9 && imc < 45.9){
+      alert('Voce esta com obesidade (Grau 1) ' + imc.toFixed(2))
+    } else if(imc >= 45.9 && imc < 59.9){
+      alert('Voce esta com obesidade (Grau 2)' + imc.toFixed(2)) 
+    } else if (imc >= 59.9 && imc < 59.9){
+      alert('Voce esta com obesidade morbida ' + imc.toFixed(2))
+    }
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Calcule seu IMC!</Text>
+
+      <TextInput placeholder="Peso (kg)" value={peso} onChangeText={(peso) => setPeso(peso)} keyboardType="numeric" style={styles.input}/>
+      <TextInput placeholder="Altura (cm)" value={altura} onChangeText={(altura) => setAltura(altura)} keyboardType="numeric" style={styles.input}/>
+   
+      <TouchableOpacity style={styles.btn} onPress={() => calcular()}>
+        <Text style={styles.btnText}>Calcular</Text>
+      </TouchableOpacity>
+   
     </View>
   );
 }
@@ -14,8 +44,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:'center',
   },
+  title:{
+    textAlign: 'center',
+    marginTop: 25,
+    fontSize: 25
+  },
+  input:{
+    backgroundColor: '#DDD',
+    borderRadius: 10,
+    margin: 15,
+    padding: 10,
+    color: '#121212',
+    fontSize: 20
+  },
+  btn:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 15,
+    backgroundColor: '#41aef4',
+    padding: 10,
+    borderRadius: 10
+  },
+  btnText:{
+    color: '#FFF',
+    fontSize: 25
+  }
 });
